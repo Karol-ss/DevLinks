8d8c85268d0924c0626ac5ee895197d86da54c0e
Karol-ss <KarolSENAI1234@gmail.com>
2023-08-10 10:55:47 -0300

    ajustes no js e css

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@ var toggleMode = () => {
     const html = document.documentElement;
 
     html.classList.toggle("light");
+
+    const  isLight = html.classList.contains("light");
+    const srcValue = isLight ? "../img/fotoPerfilClaro.png" : "../img/fotoPerfil.jpg";
+    const altValue = isLight ? "Imagem de perfil com um cachorro do telemarketing com óculos escuros" : "Imagem de um cachorro do telemarketing";
+
+
+    document.querySelector("#profile img").setAttribute("src",srcValue);
+    document.querySelector("#profile img").setAttribute("alt",altValue)
+
+
+
 };
